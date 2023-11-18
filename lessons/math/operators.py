ace633bee16d8b00aa04943c8b366e0860120029
Chris Henderson <chris@chenderson.org>
2023-11-17 16:58:44 -0800

    add assertions

--- lessons/math/operators.py
+++ lessons/math/operators.py
@@ -35,3 +35,24 @@ def divide(numerator: int, denominator: int) -> int:
 # You are NOT allowed to use the builtin division operator (/).
 def multiply(left: int, right: int):
     raise NotImplemented()
+
+
+def assert_eq(got, want):
+    assert got == want, f"got {got}, want {want}"
+
+
+assert_eq(divide(10, 2), 5)
+assert_eq(divide(11, 2), 5)
+assert_eq(divide(1, 2), 0)
+assert_eq(divide(2, 2), 1)
+assert_eq(divide(-2, 2), -1)
+assert_eq(divide(2, -2), -1)
+assert_eq(divide(-2, -2), 1)
+
+assert_eq(multiply(2, 5), 10)
+assert_eq(multiply(11, 2), 22)
+assert_eq(multiply(1, -12), -12)
+assert_eq(multiply(2, 2), 4)
+assert_eq(multiply(-2, 2), -4)
+assert_eq(multiply(2, -2), -4)
+assert_eq(multiply(-2, -2), 4)
