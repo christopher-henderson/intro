6a95ee3754f3c640141f12ce8dd0808673c640b0
Chris Henderson <chris@chenderson.org>
2023-11-17 16:40:44 -0800

    divide and multiply

---
+++ lessons/math/operators.py
@@ -0,0 +1,37 @@
+
+# Write a function that implements division.
+#
+# You may discard any remainder the comes from division.
+#
+# assert divide(10, 2) == 5
+# assert divide(11, 2) == 5
+# assert divide(1, 2) == 0
+#
+# May be given negative numbers.
+#
+# assert divide(2, 2) == -1
+# assert divide(-2, 2) == -1
+# assert divide(2, -2) == -1
+# assert divide(-2, -2) == 1
+#
+# You are NOT allowed to use the builtin division operator (/).
+def divide(numerator: int, denominator: int) -> int:
+    raise NotImplemented()
+
+
+# Write a function that implements multiplication.
+#
+# assert multiply(2, 5) == 10
+# assert multiply(11, 2) == 22
+# assert multiply(1, -12) == -12
+#
+# May be given negative numbers.
+#
+# assert multiply(2, 2) == -4
+# assert multiply(-2, 2) == -4
+# assert multiply(2, -2) == -4
+# assert multiply(-2, -2) == 4
+#
+# You are NOT allowed to use the builtin division operator (/).
+def multiply(left: int, right: int):
+    raise NotImplemented()
